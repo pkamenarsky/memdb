8a1012436b69fc7f25fff765888efe1b83bb28ab
Philip Kamenarsky <p.kamenarsky@gmail.com>
2018-07-04 16:58:34 +0200

    Extend withFile argument

--- src/Database/Immutable/Write.hs
+++ src/Database/Immutable/Write.hs
@@ -13,12 +13,13 @@ import           Control.Monad.IO.Class (MonadIO, liftIO)
 
 import qualified Data.ByteString as B
 import           Data.IORef
+import           Data.Monoid ((<>))
 import qualified Data.Serialize as S
 import           Data.Word
 
 import qualified Database.Immutable.Internal as I
 
-import           System.IO
+import           System.IO (Handle, IOMode(WriteMode))
 
 -- | Creata a database from a list. Databases can be created
 -- back using 'Database.Immutable.Read.createDB'.
@@ -37,18 +38,21 @@ writeDB
   :: S.Serialize a
   => MonadIO m
 
-  => ((Handle -> m ()) -> m ())
+  => (FilePath -> IOMode -> (Handle -> m ()) -> m ())
   -- ^ Bracketed writing function; see 'System.IO.withFile'
+
+  -> FilePath
+  -- ^ Path to write to; must be writeable
 
   -> ((a -> m ()) -> m ())
   -- ^ Output function; the continuation can be called multiple times
 
   -> m ()
-writeDB with f = do
+writeDB with path f = do
   count <- liftIO $ newIORef (0 :: Word32)
-  with $ \handle -> f $ \a -> liftIO $ do
+  with path WriteMode $ \handle -> f $ \a -> liftIO $ do
     modifyIORef count (+1)
     B.hPut handle (S.encode a)
-  with $ \handle -> liftIO $ do
+  with (path <> "." <> "meta") WriteMode $ \handle -> liftIO $ do
     count' <- readIORef count
     B.hPut handle (S.encode count')
