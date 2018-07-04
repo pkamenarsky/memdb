f6feaaa74a76c3702797bd82477679ba420d50ee
Philip Kamenarsky <p.kamenarsky@gmail.com>
2018-07-04 20:38:22 +0200

    Revert "Extend withFile argument"
    This reverts commit 8a1012436b69fc7f25fff765888efe1b83bb28ab.

--- src/Database/Immutable/Write.hs
+++ src/Database/Immutable/Write.hs
@@ -13,13 +13,12 @@ import           Control.Monad.IO.Class (MonadIO, liftIO)
 
 import qualified Data.ByteString as B
 import           Data.IORef
-import           Data.Monoid ((<>))
 import qualified Data.Serialize as S
 import           Data.Word
 
 import qualified Database.Immutable.Internal as I
 
-import           System.IO (Handle, IOMode(WriteMode))
+import           System.IO
 
 -- | Creata a database from a list. Databases can be created
 -- back using 'Database.Immutable.Read.createDB'.
@@ -38,21 +37,18 @@ writeDB
   :: S.Serialize a
   => MonadIO m
 
-  => (FilePath -> IOMode -> (Handle -> m ()) -> m ())
+  => ((Handle -> m ()) -> m ())
   -- ^ Bracketed writing function; see 'System.IO.withFile'
-
-  -> FilePath
-  -- ^ Path to write to; must be writeable
 
   -> ((a -> m ()) -> m ())
   -- ^ Output function; the continuation can be called multiple times
 
   -> m ()
-writeDB with path f = do
+writeDB with f = do
   count <- liftIO $ newIORef (0 :: Word32)
-  with path WriteMode $ \handle -> f $ \a -> liftIO $ do
+  with $ \handle -> f $ \a -> liftIO $ do
     modifyIORef count (+1)
     B.hPut handle (S.encode a)
-  with (path <> "." <> "meta") WriteMode $ \handle -> liftIO $ do
+  with $ \handle -> liftIO $ do
     count' <- readIORef count
     B.hPut handle (S.encode count')
