354d58ac6a9941f9951c57bb7ea023417e1bc2fd
Philip Kamenarsky <p.kamenarsky@gmail.com>
2020-04-15 20:30:09 +0100

    Insert

--- src/Database/Immutable/Experimental.hs
+++ src/Database/Immutable/Experimental.hs
@@ -16,15 +16,15 @@
 module Database.Immutable.Experimental where
 
 import Data.Semigroup ((<>))
-import Data.IORef (IORef, modifyIORef)
+import Data.IORef (IORef, modifyIORef, newIORef, readIORef)
 
 import qualified GHC.Generics as G
 import qualified Generics.Eot as Eot
 
 data R = Unresolved | Resolved
 
-data Id t = Id t
-data ForeignId (c :: R) t name a
+data Id t = Id t deriving Show
+data ForeignId (c :: R) t name a = ForeignId deriving Show
 
 data Person c = Person
   { pid :: Id Int
@@ -32,7 +32,7 @@ data Person c = Person
   , friend :: Maybe (ForeignId c Int "pid" Person)          -- could be turned into Maybe (Person Resolved)
   , employer :: Maybe (ForeignId c String "owner" Employer) -- could be turned into Maybe (Employer Resolved)
   , pid2 :: Id String
-  } deriving (G.Generic, Record)
+  } deriving (Show, G.Generic, Record)
 
 data Employer c = Employer
   { owner :: Id String
@@ -49,7 +49,7 @@ type family Table (c :: TableMode) a where
 data CompanyTables c = CompanyTables
   { persons :: Table c Person  -- could be turned into (DB ... -> #name -> Person Resolved)
   , employers :: Table c Employer
-  } deriving (G.Generic)
+  } deriving (G.Generic, Tables)
 
 data Index c t table
 
@@ -63,22 +63,31 @@ data DB tables indexes = DB
   , dbIndexes :: indexes
   }
 
-insert :: DB tables indexes -> tables 'Insert -> IO ()
-insert = undefined
-
 --------------------------------------------------------------------------------
 
-class GTables f tables where
-  gInsert :: f (tables 'Memory) -> f (tables 'Insert) -> IO ()
+class GTables mt it where
+  gInsert :: mt -> it -> IO ()
 
--- instance GTables f => GTables (G.D1 c f) where
---   gInsert (G.M1 m) (G.M1 i) = gInsert m i
+instance GTables () () where
+  gInsert () () = pure ()
 
-instance GTables (G.Rec0 c) tables where
-  gInsert (G.K1 m) (G.K1 i) = modifyIORef m (i <>)
+instance (GTables mt mi) => GTables (Either mt Eot.Void) (Either mi Eot.Void) where
+  gInsert (Left mt) (Left mi) = gInsert mt mi
+  gInsert _ _ = undefined
 
--- instance (G.Selector c, f ~ k p) => GTables (G.S1 c f) where
---   gInsert (G.M1 ref) (G.M1 a) = modifyIORef ref (a <>)
+instance (GTables mt mi) => GTables (IORef [a], mt) ([a], mi) where
+  gInsert (ref, mt) (as, mi) = modifyIORef ref (as <>) >> gInsert mt mi
+
+class Tables tables where
+  insert :: tables 'Memory -> tables 'Insert -> IO ()
+  default insert
+    :: Eot.HasEot (tables 'Memory)
+    => Eot.HasEot (tables 'Insert)
+    => GTables (Eot.Eot (tables 'Memory)) (Eot.Eot (tables 'Insert))
+    => tables 'Memory
+    -> tables 'Insert
+    -> IO ()
+  insert mt mi = gInsert (Eot.toEot mt) (Eot.toEot mi)
 
 --------------------------------------------------------------------------------
 
@@ -97,12 +106,15 @@ instance GRecord a => GRecord (Either a Eot.Void) where
 
 instance GRecord as => GRecord (Id String, as) where
   gGatherIds (field:fields) ((Id v), as) = EidString field v:gGatherIds fields as
+  gGatherIds _ _ = undefined
 
 instance GRecord as => GRecord (Id Int, as) where
   gGatherIds (field:fields) ((Id v), as) = EidInt field v:gGatherIds fields as
+  gGatherIds _ _ = undefined
 
 instance {-# OVERLAPPABLE #-} GRecord as => GRecord (a, as) where
   gGatherIds (_:fields) (_, as) = gGatherIds fields as
+  gGatherIds _ _ = undefined
 
 class Record a where
   gatherIds :: a -> [EId]
@@ -120,12 +132,32 @@ person = Person
   , pid2 = Id "yeah"
   }
 
+test :: [EId]
 test = gatherIds person
 
-companyTables :: DB CompanyTables ()
-companyTables = undefined
+companyTables :: CompanyTables 'Memory
+companyTables = CompanyTables
+  { persons = undefined
+  , employers = undefined
+  }
 
+testInsert :: IO ()
 testInsert = insert companyTables $ CompanyTables
   { persons = []
   , employers = []
   }
+
+testMain :: IO ()
+testMain = do
+  personsRef <- newIORef []
+  employersRef <- newIORef []
+
+  let mt = CompanyTables personsRef employersRef
+
+  insert mt $ CompanyTables
+    { persons = [person, person]
+    , employers = []
+    }
+
+  ps <- readIORef personsRef
+  print ps
